24bacfaefaea3ee069b4bfb6051db7f8f73c79d1
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2025-05-29 02:00:00 +0700

    Add @radix-ui/react-slot dependency, implement Button component, and update homepage layout with new button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default async function HomePage() {
@@ -5,6 +6,9 @@ export default async function HomePage() {
     <div className="flex flex-col gap-8 pb-8 bg-background">
       <ConnectButton />
       <p className="text-2xl font-bold">Centuari Testnet App</p>
+      <Button variant={"colorful"} size={"lg"} className="w-fit">
+        Place Order
+      </Button>
     </div>
   );
 }
