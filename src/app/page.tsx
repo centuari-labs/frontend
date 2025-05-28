aea01e197be33a98609ca26fd599dba23255caff
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2025-05-29 01:48:51 +0700

    Update layout and homepage with new font and branding, add next-themes dependency, and wrap children in ThemeProvider for improved theming support.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default async function HomePage() {
   return (
-    <div className="flex flex-col gap-8 pb-8">
+    <div className="flex flex-col gap-8 pb-8 bg-background">
       <ConnectButton />
+      <p className="text-2xl font-bold">Centuari Testnet App</p>
     </div>
   );
 }
--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { config } from "@/lib/wagmi";
 import "@rainbow-me/rainbowkit/styles.css";
+import { ThemeProvider } from "./theme-provider";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +14,11 @@ export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={darkTheme()}>{children}</RainbowKitProvider>
+        <RainbowKitProvider theme={darkTheme()}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            {children}
+          </ThemeProvider>
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
